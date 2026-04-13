d1bca88ce02045abcb411820377eadea6cc777f2
Swetha <swethataidsb25@gmail.com>
2026-04-13 16:51:11 +0530

    updated task 2 ,3 and 4

--- task4/Main.java
+++ task4/Main.java
@@ -115,7 +115,7 @@ class StudentManager {
 }
 
 // main class
-public class Main {
+public class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         StudentManager sm = new StudentManager();
